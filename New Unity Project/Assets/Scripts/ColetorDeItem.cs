cbedc6c1108cf25278235b62995a63ecf77afb2c
Tesla <ederkbb@gmail.com>
2020-03-06 21:25:39 -0400

    Coletor de Item \ Separador de Materias

---
+++ New Unity Project/Assets/Scripts/ColetorDeItem.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+
+public class ColetorDeItem : MonoBehaviour
+{
+    private GameObject gc;
+    bool jaColetou;
+
+    public enum Material
+    {
+        plastico,
+        papel,
+        vidro,
+        metal
+    };
+
+    public Material mat = new Material();
+
+    void Start()
+    {
+        gc = GameObject.Find("GameController");
+        jaColetou = false;
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (mat == Material.plastico && !jaColetou)
+                {
+                    gc.GetComponent<GameController>().ColetarPlastico();
+                    jaColetou = true;
+                    Destroy(this.gameObject, 1.0f);
+                }
+                if (mat == Material.papel && !jaColetou)
+                {
+                    gc.GetComponent<GameController>().ColetarPapel();
+                    jaColetou = true;
+                    Destroy(this.gameObject, 1.0f);
+                }
+                if (mat == Material.vidro && !jaColetou)
+                {
+                    gc.GetComponent<GameController>().ColetarVidro();
+                    jaColetou = true;
+                    Destroy(this.gameObject, 1.3f);
+                }
+                if (mat == Material.metal && !jaColetou)
+                {
+                    gc.GetComponent<GameController>().ColetarMetal();
+                    jaColetou = true;
+                    Destroy(this.gameObject, 1.2f);
+                }
+            }
+        }
+    }
+
+}
---
+++ New Unity Project/Assets/Scripts/SepararMateriais.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SepararMateriais : MonoBehaviour
+{
+    private GameObject gc;
+
+    public Text separarPlastico;
+    public Text separarPapel;
+    public Text separarVidro;
+    public Text separarMetal;
+
+    public enum Material
+    {
+        plastico,
+        papel,
+        vidro,
+        metal
+    };
+
+    public Material mat = new Material();
+
+    void Awake()
+    {
+        separarMetal.enabled = false;
+        separarPapel.enabled = false;
+        separarVidro.enabled = false;
+        separarPlastico.enabled = false;
+    }
+
+    IEnumerator AvisoMaterial()
+    {
+        if (mat == Material.plastico)
+        {
+            separarPlastico.enabled = true;
+            yield return new WaitForSeconds(2.5f);
+            separarMetal.enabled = false;
+        }
+        if (mat == Material.metal)
+        {
+            separarMetal.enabled = true;
+            yield return new WaitForSeconds(2.5f);
+            separarMetal.enabled = false;
+        }
+        if (mat == Material.papel)
+        {
+            separarPapel.enabled = true;
+            yield return new WaitForSeconds(2.5f);
+            separarMetal.enabled = false;
+        }
+        if (mat == Material.vidro)
+        {
+            separarVidro.enabled = true;
+            yield return new WaitForSeconds(2.5f);
+            separarMetal.enabled = false;
+        }
+    }
+    void Start()
+    {
+        gc = GameObject.Find("GameController");
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (mat == Material.plastico && gc.GetComponent<GameController>().plastico > 0)
+                {
+                    gc.GetComponent<GameController>().SepararPlastico();
+                    gc.GetComponent<GameController>().ZerarPlastico();
+                    Debug.Log("plastico");
+                    StartCoroutine(AvisoMaterial());
+                }
+                if (mat == Material.papel && gc.GetComponent<GameController>().papel > 0)
+                {
+                    gc.GetComponent<GameController>().SepararPapel();
+                    gc.GetComponent<GameController>().ZerarPapel();
+                    StartCoroutine(AvisoMaterial());
+                }
+                if (mat == Material.vidro && gc.GetComponent<GameController>().vidro > 0)
+                {
+                    gc.GetComponent<GameController>().SepararVidro();
+                    gc.GetComponent<GameController>().ZerarVidro();
+                    StartCoroutine(AvisoMaterial());
+                }
+                if (mat == Material.metal && gc.GetComponent<GameController>().metal > 0)
+                {
+                    gc.GetComponent<GameController>().SepararMetal();
+                    gc.GetComponent<GameController>().ZerarMetal();
+                    StartCoroutine(AvisoMaterial());
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
